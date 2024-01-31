b7347d828cb6eb501b7af48efd960078542b072d
Faruk Karagoz <faruk.karagoz07@outlook.com>
2024-01-30 20:11:20 -0700

    IGeneric Reposity contracts defined

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        //productRepositry.where(x=>x.id>).OrderBy.ToListAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+    }
+}
