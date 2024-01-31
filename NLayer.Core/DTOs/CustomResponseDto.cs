40ed8dedc769c1b4ddf82b3f71d993521fbf13f6
Faruk Karagoz <faruk.karagoz07@outlook.com>
2024-01-31 13:46:35 -0700

    CustomResponseDto Implemented

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    internal class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCdoe { get; set; }
+        public List<String> Errors { get; set; }
+
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDto<T>
+            {
+                Data = data,
+                StatusCdoe = statusCode
+                
+            };
+        }
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            return new CustomResponseDto<T>
+            {
+                
+                StatusCdoe = statusCode
+               
+            };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+        {
+            return new CustomResponseDto<T>
+            {
+                
+                StatusCdoe = statusCode,
+                Errors = errors
+            };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDto<T>
+            {
+
+                StatusCdoe = statusCode,
+                Errors  = new List<string> { error }
+            };
+        }
+    }
+
+
+}
