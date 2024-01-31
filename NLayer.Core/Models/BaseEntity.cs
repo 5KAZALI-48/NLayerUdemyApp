df452713faa64bf502afbfe19b49d5bcc741b458
Faruk Karagoz <faruk.karagoz07@outlook.com>
2024-01-31 13:14:56 -0700

    Relocation of entities Base Dto and Product Dto created

--- NLayer.Core/Models/BaseEntity.cs
+++ NLayer.Core/Models/BaseEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NLayer.Core;
+namespace NLayer.Core.Models;
 
 //cannot be instantiated 
 public abstract class BaseEntity
