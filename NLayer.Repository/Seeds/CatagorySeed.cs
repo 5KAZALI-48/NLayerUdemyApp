a9c9214f87412ba0319eaf604951b7f46f71307e
Faruk Karagoz <faruk.karagoz07@outlook.com>
2024-01-31 10:34:28 -0700

    Seed Catagory

---
+++ NLayer.Repository/Seeds/CatagorySeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class CatagorySeed : IEntityTypeConfiguration<Catagory>
+    {
+        public void Configure(EntityTypeBuilder<Catagory> builder)
+        {
+            builder.HasData(new Catagory { Id = 1, Name = "Kalemler" },
+                            new Catagory { Id = 2, Name = "Kitaplar" },
+                            new Catagory { Id = 3, Name = "Defterler" });
+        }
+    }
+}
